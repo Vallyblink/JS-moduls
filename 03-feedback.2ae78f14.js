7c42c5bfccc712d35eb81fb98aeba1ccab35ca5c
Vallyblink <115736687+Vallyblink@users.noreply.github.com>
2023-03-27 23:07:19 +0000

    Deploying to gh-pages from @ Vallyblink/goit-js-hw-08@3fd071435082a436e65bc8efd282f5661dde651b 🚀

--- 03-feedback.2ae78f14.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("input[name=email]"),t=document.querySelector("textarea[name=message]");document.querySelector("button[type=submit]");function a(a){a.preventDefault();let n=e.value,u=t.value;localStorage.setItem("feedback-form-state",`Email = ${n}; Message = ${u}`)}e.addEventListener("input",a),t.addEventListener("input",a);
-//# sourceMappingURL=03-feedback.2ae78f14.js.map
