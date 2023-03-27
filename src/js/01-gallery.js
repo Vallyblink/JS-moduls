3fd071435082a436e65bc8efd282f5661dde651b
Vallyblink <alabama.toj@gmail.com>
2023-03-28 02:06:32 +0300

    last

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,6 +4,8 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+console.log( pictureCard)
+
 console.log(galleryItems);
 const galleryList = document.querySelector(".gallery")
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,38 @@
+import throttle from "lodash.throttle";
 
-const emailInput = document.querySelector( "input[name=email]");
-const messageInput = document.querySelector("textarea[name=message]");
-const submitButton = document.querySelector("button[type=submit]");
+const refs ={
+  form : document.querySelector(".feedback-form"),
+  input: document.querySelector( "input[name=email]"),
+  textarea: document.querySelector("textarea[name=message]"),
+}
+ let dataForm = {};
+ const MESSAGE_KEY = 'feedback-form-state';
+ onLocalCheck()
 
-emailInput.addEventListener("input", onValueSafe)
-messageInput.addEventListener("input", onValueSafe)
+refs.form.addEventListener("input", throttle(onLocalStorageSafe, 500));
+refs.form.addEventListener("submit", onFormSubmit);
 
-function onValueSafe (event){
-  event.preventDefault();
-  let email = emailInput.value;
-  let message = messageInput.value;
-  localStorage.setItem('feedback-form-state',`Email = ${email}; Message = ${message}`)
+function onLocalStorageSafe  (el) {
+  dataForm[el.target.name] = el.target.value;
+  localStorage.setItem(MESSAGE_KEY, JSON.stringify(dataForm))
+}
+ 
+function onFormSubmit (evt){
+  evt.preventDefault();
+  if(refs.input.value === "" || refs.textarea.value == ""){
+    alert (" Can you fill the fields? Awaiting for your opinion")
+    return
+  }
+   evt.currentTarget.reset();
+   console.log( dataForm)
+   localStorage.removeItem(MESSAGE_KEY);
+}
 
+function onLocalCheck (e){
+  if ( localStorage.getItem(MESSAGE_KEY)){
+    dataForm = JSON.parse(localStorage.getItem(MESSAGE_KEY));
+    for(const key in dataForm){
+      refs.form.elements[key].value = dataForm[key]
+    }
+  }
 }
